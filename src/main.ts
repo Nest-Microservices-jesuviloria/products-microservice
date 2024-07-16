2327dceff6e9fe87ff5c49f76fc78b341efcbabb
jesuviloria <jesudvm12@gmail.com>
2024-07-16 17:54:17 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /* transform: true, */
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      /* disableErrorMessages: false, */
+    }),
+  );
+
+  logger.log(`Products Microservice running on port ${envs.port}`);
+}
+bootstrap();
